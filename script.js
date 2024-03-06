b9d6d70c9b7b7efd3bc7b695c473faf8cf833591
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2024-03-06 13:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const p = document.querySelector("#status");
+const btn = document.querySelector("#enterBtn");
+
+btn.addEventListener("click",()=>{
+    console.log(p.tagName);
+	p.innerHTML = "<h1>Enter the Metaverse<h1>";
+})
